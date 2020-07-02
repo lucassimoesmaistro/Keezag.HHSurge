e1494c01519227d3427d843a4ab0ff1365a12b3f
Lucas Simões <lucassimoes_@hotmail.com>
2020-07-02 00:07:55 -0300

    ok

--- src/Keezag.HHSurge.Api/Program.cs
+++ src/Keezag.HHSurge.Api/Program.cs
@@ -12,22 +12,16 @@ namespace Keezag.HHSurge.Api
 {
     public class Program
     {
-
         public static void Main(string[] args)
         {
-            var config = new ConfigurationBuilder()
-                .AddJsonFile("hosting.json", optional: true)
-                .Build();
-
-            BuildWebHost(args, config).Run();
+            CreateHostBuilder(args).Build().Run();
         }
 
-        public static IWebHost BuildWebHost(string[] args, IConfiguration configurationBuilder) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseIISIntegration()
-                .UseKestrel()
-                .UseConfiguration(configurationBuilder)
-                .UseStartup<Startup>()
-                .Build();
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
